b33fe30ba5725609d925e43c199c1481ffb73039
joquebedebarboza-ux <joquebedebarboza@gmail.com>
2026-08-12 15:37:19 -0400

    Delete MatriculaTec/src/br/edu/cetam/matriculatec directory

--- MatriculaTec/src/br/edu/cetam/matriculatec/exception/MatriculaDuplicadaException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.cetam.matriculatec.exception;
-
-public class MatriculaDuplicadaException extends Exception {
-    public MatriculaDuplicadaException(String mensagem) {
-        super(mensagem);
-    }
-}
