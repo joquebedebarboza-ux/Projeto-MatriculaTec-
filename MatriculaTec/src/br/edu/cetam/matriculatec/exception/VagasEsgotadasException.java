e85d87b6e98d8d0af62d27940b6e4f2fcac2c751
joquebedebarboza-ux <joquebedebarboza@gmail.com>
2026-08-12 15:29:16 -0400

    Delete MatriculaTec directory

--- MatriculaTec/src/br/edu/cetam/matriculatec/exception/VagasEsgotadasException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.cetam.matriculatec.exception;
-
-public class VagasEsgotadasException extends Exception {
-    public VagasEsgotadasException(String mensagem) {
-        super(mensagem);
-    }
-}
--- MatriculaTec/src/br/edu/cetam/matriculatec/model/interfaces/RelatorioExportavel.java
+++
@@ -1,5 +0,0 @@
-package br.edu.cetam.matriculatec.model.interfaces;
-
-public interface RelatorioExportavel {
-    String gerarRelatorio();
-}
